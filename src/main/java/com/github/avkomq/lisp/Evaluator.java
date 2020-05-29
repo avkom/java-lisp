5514cfb9fbf71283ee1ffaa15620d54dcd5322dd
Oleksandr Komendant <avkomq@gmail.com>
2020-05-29 00:15:07 -0400

    Implement Evaluator.evaluate() for + operator

--- src/main/java/com/github/avkomq/lisp/Evaluator.java
+++ src/main/java/com/github/avkomq/lisp/Evaluator.java
@@ -1,7 +1,39 @@
 package com.github.avkomq.lisp;
 
+import java.util.ArrayList;
+
 public class Evaluator {
     public Object evaluate(Object ast, Environment environment) {
-        return ast;
+        if (ast instanceof String ||
+            ast instanceof Number ||
+            ast instanceof Boolean ||
+            ast == null) {
+            return ast;
+        }
+
+        if (ast instanceof Symbol) {
+            return environment.get((Symbol) ast);
+        }
+
+        if (ast instanceof ArrayList) {
+            ArrayList<Object> list = (ArrayList) ast;
+            Object head = list.get(0);
+
+            if ("define".equals(head)) {
+                Symbol symbol = (Symbol) list.get(1);
+                Object value = evaluate(list.get(2), environment);
+                environment.set(symbol, value);
+                return value;
+            }
+
+            Lambda lambda = (Lambda) evaluate(head, environment);
+            ArrayList<Object> args = new ArrayList<>();
+            for (int i = 1; i < list.size(); i++) {
+                args.add(evaluate(list.get(i), environment));
+            }
+            return lambda.Invoke(args.toArray());
+        }
+
+        return null;
     }
 }
